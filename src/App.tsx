189605f26afe8a2ba11c1e5db56645879a4abbf4
egorkharlamov <egor.kharlamov@ifellow.ru>
2026-02-27 23:26:03 +0700

    feat: add statscard and fill them in with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import './App.css'
 import {Layout} from "./components/layout/Layout.tsx";
+import {StatsCard} from "./components/dashboard/StatsCard.tsx";
+import {useStats} from "./api/queries.ts";
 
 function App() {
-  return (
+    const { data: stats} = useStats();
+    //TODO: add icons to cards
+    return (
       <Layout>
-          <div>
-              Dashboard content here
+          <div className="grid grid-cols-4 gap-8">
+              <StatsCard
+                  title="Revenue"
+                  value={stats ? stats.revenue.toString() : ""}
+                  change={stats ? stats.revenueChange : 0}
+              />
+              <StatsCard
+                  title="Users"
+                  value={stats ? stats.users.toString() : ""}
+                  change={stats ? stats.usersChange : 0}
+              />
+              <StatsCard
+                  title="Orders"
+                  value={stats ? stats.orders.toString() : ""}
+                  change={stats ? stats.ordersChange : 0}
+              />
+              <StatsCard
+                  title="Rate"
+                  value={stats ? stats.conversion.toString() : ""}
+                  change={stats ? stats.conversionChange : 0}
+              />
           </div>
       </Layout>
   )
