318f20abd55ce142151eeda645674e6b78d02526
egorkharlamov <egor.kharlamov@ifellow.ru>
2026-03-03 22:12:42 +0700

    bugfix: fix covering of "status" Dropdown for table, remove useless useEffect

--- src/components/dashboard/Transactions.tsx
+++ src/components/dashboard/Transactions.tsx
@@ -50,8 +50,8 @@ export const Transactions:FC<ITransactionsArr> = ({data}) => {
             })
         } else if (type === "amount") {
             sortedDataInTable = [...sortedData].sort((a,b) => {
-                const aAm = new Date(a.amount);
-                const bAm = new Date(b.amount);
+                const aAm = a.amount;
+                const bAm = b.amount;
 
                 if (aAm > bAm) return direction === "asc" ? -1 : 1;
                 if (aAm < bAm) return direction === "asc" ? 1 : -1;
@@ -75,15 +75,11 @@ export const Transactions:FC<ITransactionsArr> = ({data}) => {
     }, [data]);
 
     useEffect(() => {
-        setSortDirection("");
-    }, []);
-
-    useEffect(() => {
         sortTableByStatus(chooseStatus)
     }, [chooseStatus]);
 
     return (
-        <div className="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default">
+        <div className="relative bg-neutral-primary-soft shadow-xs rounded-base border border-default">
             <table className="w-full text-sm text-left rtl:text-right text-body">
                 <thead className="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
                 <tr>
