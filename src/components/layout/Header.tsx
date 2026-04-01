75e64ffbfa5e2c8da786299d530717204885eb9e
egorkharlamov <egor.kharlamov@ifellow.ru>
2026-04-01 17:51:36 +0700

    fix: add responsive-design for header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,16 +5,16 @@ export const Header = () => {
     const updateDate = `${new Date().toLocaleDateString("en-US", {month: 'short', day: "numeric"})} ${new Date().toLocaleTimeString()}`
     return (
         <header className="bg-white">
-            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-center lg:justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1 items-center">
                     <a href="#" className="-m-1.5 p-1.5">
                         <img
                             alt=""
                             src={Logo}
-                            className="h-12 w-auto"
+                            className="h-20 w-auto"
                         />
                     </a>
-                    <h1 className="text-lg font-bold text-gray-900">Analytics Dashboard</h1>
+                    <h1 className="ml-2 font-bold text-gray-900 hidden md:block">Analytics Dashboard</h1>
                 </div>
 
                 <div className="hidden lg:flex-col lg:flex lg:flex-1 lg:justify-end">
